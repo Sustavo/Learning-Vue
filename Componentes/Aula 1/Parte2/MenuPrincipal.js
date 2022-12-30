e383a0572abacb2396ea03e75d8ae157a013cb94
Gustavo <gust4v0309@gmail.com>
2022-12-29 21:13:15 -0300

    feat: MenuPrincipal aula1 pt2

---
+++ Componentes/Aula 1/Parte2/MenuPrincipal.js
@@ -0,0 +1,18 @@
+// import BotaoContador from "./BotaoContador.js";
+
+const MenuPrincipal = {
+    name: "MenuPrincipal",
+    template: `
+        <ul>
+            <li>Home</li>
+            <li>Contato</li>
+            <botao-contador></botao-contador>
+        </ul>
+
+    `,
+    // components: {
+    //     BotaoContador
+    // }
+}
+
+export default MenuPrincipal;
