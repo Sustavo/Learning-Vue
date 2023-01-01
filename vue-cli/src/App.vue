b6e0f788123af8f9c7fcbe03ed7f972325c2fc7e
Gustavo <gust4v0309@gmail.com>
2023-01-01 20:17:04 -0300

    Feat: Adjuste App.vue

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <HeaderPrincipal />
+  </div>
+</template>
+
+<script>
+import HeaderPrincipal from './components/HeaderPrincipal.vue';
+
+
+export default {
+  components: {
+    HeaderPrincipal
+  },
+}
+</script>
+
+<style>
+
+</style>
