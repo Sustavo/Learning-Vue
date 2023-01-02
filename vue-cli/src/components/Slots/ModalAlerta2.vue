89867c39e3a4427a77d0a58f376d9f26480efd44
Gustavo <gust4v0309@gmail.com>
2023-01-02 17:52:36 -0300

    Feat: ModalAlerta2.vue

---
+++ vue-cli/src/components/Slots/ModalAlerta2.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="modal">
+    <button>Fechar</button>
+    <slot :user="user"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "ModalAlerta",
+    data() {
+        return{
+            user: {
+                name: "Andŕe"
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
