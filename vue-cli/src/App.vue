58b8fa10d5e36cf97c1976e1bffc15674e7edb61
Gustavo <gust4v0309@gmail.com>
2023-01-02 18:08:48 -0300

    calling async components

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -15,13 +15,14 @@
         <template v-slot:default="slotProps">{{ slotProps }}</template>
     </ModalAlerta2> -->
 
-    <!-- <button @click="componenteAtivo = 'AulaDescricao'" >Descricao</button>
+    <button @click="componenteAtivo = 'AulaDescricao'" >Descricao</button>
     <button @click="componenteAtivo = 'AulaDownloads'" >Downloads</button>
+
     <keep-alive>
       <component :is="componenteAtivo"></component>
-    </keep-alive> -->
+    </keep-alive>
 
-    <div class="menu">
+    <!-- <div class="menu">
       <button @click="componenteMenu = 'FalandoEmpresa'">Serviços</button>
       <button @click="componenteMenu = 'ServicoEmpresa'">Contato</button>
       <button @click="componenteMenu = 'FormularioContato'">Quem Somos</button>
@@ -29,7 +30,7 @@
 
     <keep-alive>
       <component :is="componenteMenu"></component>
-    </keep-alive>
+    </keep-alive> -->
 
 
   </div>
@@ -38,11 +39,11 @@
 <script>
   // import ModalAlerta from './components/Slots/ModalAlerta.vue';
   // import ModalAlerta2 from './components/Slots/ModalAlerta2.vue';
-  // import AulaDescricao from './components/Dynamic/AulaDescricao.vue';
+  import AulaDescricao from './components/Dynamic/AulaDescricao.vue';
   // import AulaDownloads from './components/Dynamic/AulaDownloads.vue';
-  import FalandoEmpresa from './components/Exercícios/dynamic/FalandoEmpresa.vue'
-  import ServicoEmpresa from './components/Exercícios/dynamic/ServicoEmpresa.vue'
-  import FormularioContato from './components/Exercícios/dynamic/FormularioContato.vue'
+  // import FalandoEmpresa from './components/Exercícios/dynamic/FalandoEmpresa.vue'
+  // import ServicoEmpresa from './components/Exercícios/dynamic/ServicoEmpresa.vue'
+  // import FormularioContato from './components/Exercícios/dynamic/FormularioContato.vue'
   
 
   export default {
@@ -50,17 +51,17 @@
     components: {
       // ModalAlerta,
       // ModalAlerta2,
-      // AulaDescricao,
-      // AulaDownloads,
-      FalandoEmpresa,
-      ServicoEmpresa,
-      FormularioContato,
+      AulaDescricao,
+      AulaDownloads: () => import("./components/Dynamic/AulaDownloads.vue"), //Deixando a chamda de forma asíncrona
+      // FalandoEmpresa,
+      // ServicoEmpresa,
+      // FormularioContato,
       
     },
     data() {
       return {
-        // componenteAtivo: "AulaDescricao"
-        componenteMenu: "FalandoEmpresa"
+        componenteAtivo: "AulaDescricao",
+        // componenteMenu: "FalandoEmpresa"
       }
     }
   };
