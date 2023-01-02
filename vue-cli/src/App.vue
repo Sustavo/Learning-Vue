e42a84ab514dd638ac9e36f0e42be7dba9119c56
Gustavo <gust4v0309@gmail.com>
2023-01-02 20:14:14 -0300

    Feat: app vue (transiçao feita por components)

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -14,14 +14,13 @@
     <ModalAlerta2>
         <template v-slot:default="slotProps">{{ slotProps }}</template>
     </ModalAlerta2> -->
-
+<!-- 
     <button @click="componenteAtivo = 'AulaDescricao'" >Descricao</button>
     <button @click="componenteAtivo = 'AulaDownloads'" >Downloads</button>
 
     <keep-alive>
       <component :is="componenteAtivo"></component>
-    </keep-alive>
-
+    </keep-alive> -->
     <!-- <div class="menu">
       <button @click="componenteMenu = 'FalandoEmpresa'">Serviços</button>
       <button @click="componenteMenu = 'ServicoEmpresa'">Contato</button>
@@ -32,6 +31,15 @@
       <component :is="componenteMenu"></component>
     </keep-alive> -->
 
+    <!-- <TransitionEstoque /> -->
+
+    <ul>
+      <li @click="componenteAtivo = 'FalandoEmpresa'">Sobre</li>
+      <li @click="componenteAtivo = 'ServicoEmpresa'">Serviços</li>
+    </ul>
+    <transition name="fade" mode="out-in">
+      <component :is="componenteAtivo"></component>
+    </transition>
 
   </div>
 </template>
@@ -39,11 +47,16 @@
 <script>
   // import ModalAlerta from './components/Slots/ModalAlerta.vue';
   // import ModalAlerta2 from './components/Slots/ModalAlerta2.vue';
-  import AulaDescricao from './components/Dynamic/AulaDescricao.vue';
+  // import AulaDescricao from './components/Dynamic/AulaDescricao.vue';
   // import AulaDownloads from './components/Dynamic/AulaDownloads.vue';
-  // import FalandoEmpresa from './components/Exercícios/dynamic/FalandoEmpresa.vue'
-  // import ServicoEmpresa from './components/Exercícios/dynamic/ServicoEmpresa.vue'
+  import FalandoEmpresa from './components/Exercícios/dynamic/FalandoEmpresa.vue'
+  import ServicoEmpresa from './components/Exercícios/dynamic/ServicoEmpresa.vue'
   // import FormularioContato from './components/Exercícios/dynamic/FormularioContato.vue'
+  // import TransitionAnimation from './components/Animacoes/TransitionAnimation.vue'
+  // import TransitionAppear from './components/Animacoes/Transition/TransitionAppear.vue';
+  // import TransitionExerc from './components/Exercícios/Transition/TransitionExerc.vue';
+  // import TransitionHooks from './components/Animacoes/Transition/TransitionHooks.vue'
+  // import TransitionEstoque from './components/Animacoes/Components/TransitionEstoque.vue';
   
 
   export default {
@@ -51,16 +64,20 @@
     components: {
       // ModalAlerta,
       // ModalAlerta2,
-      AulaDescricao,
-      AulaDownloads: () => import("./components/Dynamic/AulaDownloads.vue"), //Deixando a chamda de forma asíncrona
-      // FalandoEmpresa,
-      // ServicoEmpresa,
+      // AulaDescricao,
+      // AulaDownloads: () => import("./components/Dynamic/AulaDownloads.vue"), //Deixando a chamda de forma asíncrona
+      FalandoEmpresa,
+      ServicoEmpresa,
       // FormularioContato,
-      
+      // TransitionAnimation,
+      // TransitionAppear,
+      // TransitionExerc,
+      // TransitionHooks,
+      // TransitionEstoque,
     },
     data() {
       return {
-        componenteAtivo: "AulaDescricao",
+        componenteAtivo: "FalandoEmpresa",
         // componenteMenu: "FalandoEmpresa"
       }
     }
@@ -68,5 +85,15 @@
 </script>
 
 <style>
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity .5s;
+  }
+
+  .fade-enter,
+  .fade-leave-to {
+    opacity: 0;
+  }
 
 </style>
