e5f360ea47eb20c9d5c67b3fb2b48e866b718239
Gustavo <gust4v0309@gmail.com>
2023-01-02 17:52:11 -0300

    feat: FalandoEmpresa.vue

---
+++ vue-cli/src/components/Exercícios/dynamic/FalandoEmpresa.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>Technomart é uma empresa de marketing digital, agenciamos grandes influenciadores</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'FalandoEmpresa'
+}
+</script>
+
+<style>
+
+</style>
