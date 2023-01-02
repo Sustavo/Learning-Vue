43ca37991ce5246f9fc0e9bdb521d430997b00d2
Gustavo <gust4v0309@gmail.com>
2023-01-02 17:52:29 -0300

    Feat: ModalAlerta.vue

---
+++ vue-cli/src/components/Slots/ModalAlerta.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="modal">
+    <header>
+        <slot name="header"></slot>
+    </header>
+    <section>
+        <slot>Fallback se não tiver Conteúdo</slot>
+    </section>
+    <footer>
+        <slot name="footer"></slot>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "ModalAlerta"
+};
+</script>
+
+<style>
+
+    header {
+        border-bottom: 2px solid;
+    }
+
+    section {
+        margin: 20px;
+    }
+
+    footer {
+        border-top: 2px solid;
+    }
+
+</style>
