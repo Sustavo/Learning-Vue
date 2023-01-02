7eb145652357ba0e4d90cec3f7cc59617d4fb994
Gustavo <gust4v0309@gmail.com>
2023-01-02 20:14:34 -0300

    Feat: TransitionAppear.vue

---
+++ vue-cli/src/components/Animacoes/Transition/TransitionAppear.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <transition appear>
+        <h1>Serviços</h1>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'TransitionAppear'
+}
+</script>
+
+<style>
+    .v-enter-active,
+    .v-leave-active {
+    transition: opacity 2s;
+    }
+    .v-enter,
+    .v-leave-to {
+    opacity: 0;
+    }
+</style>
