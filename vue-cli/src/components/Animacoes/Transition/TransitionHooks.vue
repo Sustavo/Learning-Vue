64b3eeb172451a39c32e0b18e2ef845ba995c2fb
Gustavo <gust4v0309@gmail.com>
2023-01-02 20:14:41 -0300

    Feat: TransitionHooks.vue

---
+++ vue-cli/src/components/Animacoes/Transition/TransitionHooks.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <button @click="ativo = !ativo">Ativar</button>
+    <transition name="fade" @before-enter="enter" @enter="beforeEnter" @before-leave="beforeLeave" @leave="leave" >
+        <h1 v-if="ativo">Serviços</h1>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "TransitionHooks",
+    data() {
+        return {
+            ativo: false
+        };
+    },
+    methods: {
+        enter() {
+            console.log("Olá :) hehe");
+        },
+        beforeEnter() {
+            console.log("Seja bem vindo :)")
+        },
+        beforeLeave() {
+            console.log("Tchau :C Not hehe")
+        },
+        leave() {
+            console.log("Até mais :(")
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
