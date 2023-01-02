63f6fde8fc8d35faf14dd9afacf4abba02c9ef06
Gustavo <gust4v0309@gmail.com>
2023-01-02 20:14:48 -0300

    Feat: TransitionExerc.vue

---
+++ vue-cli/src/components/Exercícios/Transition/TransitionExerc.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="modal">
+    <button @click="ativo = !ativo">Aparecer/Desaparecer</button>
+    <transition name="fade">
+        <h1 v-if="ativo">Boa noite :)</h1>
+    </transition>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'TransitionExerc',
+    data() {
+        return {
+            ativo: false
+        }
+    }
+}
+</script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 2s;
+}
+
+.fade-enter,
+.fade-leave-to {
+    opacity: 0;
+}
+
+</style>
