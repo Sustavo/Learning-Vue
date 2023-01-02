9c1898903380e97289b88ccec4e2cfdc7ccba6f3
Gustavo <gust4v0309@gmail.com>
2023-01-02 17:52:16 -0300

    feat: FormularioContato.vue

---
+++ vue-cli/src/components/Exercícios/dynamic/FormularioContato.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <p>Contatos:</p>
+      <p>Número: 85 9 97617221</p>
+      <p>Email: gust4v0309@gmail.com</p>
+      <p>Endereço: Rua guarani 617</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'FormularioContato'
+  }
+  </script>
+  
+  <style>
+  
+  </style>
