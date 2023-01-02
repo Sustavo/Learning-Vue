d412b41ff618b63503cac97e9b54c5bd20927c13
Gustavo <gust4v0309@gmail.com>
2023-01-02 17:51:51 -0300

    Feat: App.vue modify

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,18 +1,69 @@
+<!-- ScopedSlots -->
 <template>
   <div id="app">
-    <HeaderPrincipal />
+    <!-- <ModalAlerta>
+      <template v-slot:header>
+        <h1>Header do Slot</h1>
+      </template>
+      <button>Comprar</button>
+      <template v-slot:footer>
+        <p>Esse é o footer do slot.</p>
+      </template>
+    </ModalAlerta>
+
+    <ModalAlerta2>
+        <template v-slot:default="slotProps">{{ slotProps }}</template>
+    </ModalAlerta2> -->
+
+    <!-- <button @click="componenteAtivo = 'AulaDescricao'" >Descricao</button>
+    <button @click="componenteAtivo = 'AulaDownloads'" >Downloads</button>
+    <keep-alive>
+      <component :is="componenteAtivo"></component>
+    </keep-alive> -->
+
+    <div class="menu">
+      <button @click="componenteMenu = 'FalandoEmpresa'">Serviços</button>
+      <button @click="componenteMenu = 'ServicoEmpresa'">Contato</button>
+      <button @click="componenteMenu = 'FormularioContato'">Quem Somos</button>
+    </div>
+
+    <keep-alive>
+      <component :is="componenteMenu"></component>
+    </keep-alive>
+
+
   </div>
 </template>
 
 <script>
-import HeaderPrincipal from './components/HeaderPrincipal.vue';
+  // import ModalAlerta from './components/Slots/ModalAlerta.vue';
+  // import ModalAlerta2 from './components/Slots/ModalAlerta2.vue';
+  // import AulaDescricao from './components/Dynamic/AulaDescricao.vue';
+  // import AulaDownloads from './components/Dynamic/AulaDownloads.vue';
+  import FalandoEmpresa from './components/Exercícios/dynamic/FalandoEmpresa.vue'
+  import ServicoEmpresa from './components/Exercícios/dynamic/ServicoEmpresa.vue'
+  import FormularioContato from './components/Exercícios/dynamic/FormularioContato.vue'
+  
 
-
-export default {
-  components: {
-    HeaderPrincipal
-  },
-}
+  export default {
+    name: 'app',
+    components: {
+      // ModalAlerta,
+      // ModalAlerta2,
+      // AulaDescricao,
+      // AulaDownloads,
+      FalandoEmpresa,
+      ServicoEmpresa,
+      FormularioContato,
+      
+    },
+    data() {
+      return {
+        // componenteAtivo: "AulaDescricao"
+        componenteMenu: "FalandoEmpresa"
+      }
+    }
+  };
 </script>
 
 <style>
