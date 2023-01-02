e817b6572d4a7e272e28971e83547962377b572b
Gustavo <gust4v0309@gmail.com>
2023-01-02 17:52:05 -0300

    feat: AulaDownloads.vue

---
+++ vue-cli/src/components/Dynamic/AulaDownloads.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Faça o download dos arquivos da aula</p>
+    <button>Arquivos</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "AulaDownloads"
+}
+</script>
+
+<style>
+
+</style>
