82c5495f34bae9bc1966605cb3d9b4e100aeb4da
Gustavo <gust4v0309@gmail.com>
2023-01-04 23:28:59 -0300

    App vue and main

--- vue-router/src/App.vue
+++ vue-router/src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-
+    <nav>
+      <!-- Define o link para o router -->
+      <router-link to="/">Home</router-link>
+      <router-link to="/sobre">Cursos</router-link>
+    </nav>
+    <!-- Local em que o componente aparecerá -->
+    <router-view></router-view>
   </div>
 </template>
 
