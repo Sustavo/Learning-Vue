916a80a346210fd95094c3ab49f7b9b0b7a92531
Gustavo <gust4v0309@gmail.com>
2023-01-04 23:29:20 -0300

    Feat: Create Sobre.vue

---
+++ vue-router/src/views/Sobre.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        Sobre
+    </div>
+</template>
+
+<!-- eslint-disable vue/multi-word-component-names -->
+<script>
+    export default {
+        name: "Sobre"
+    }
+</script>
+
+<style>
+
+</style>
