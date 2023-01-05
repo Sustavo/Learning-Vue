2edb90f9f39e92644ce12a2344e1d47f4e0f29a1
Gustavo <gust4v0309@gmail.com>
2023-01-04 23:29:13 -0300

    feat: Create Home.vue

---
+++ vue-router/src/views/Home.vue
@@ -0,0 +1,14 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <h1>Essa é a home</h1>
+</template>
+
+<!-- eslint-disable vue/multi-word-component-names -->
+<script>
+export default {
+  name: 'Home'
+};
+</script>
+
+<style>
+</style>
