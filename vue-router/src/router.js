3ea3774672ee63e082e0be17364acf4c71b73984
Gustavo <gust4v0309@gmail.com>
2023-01-04 23:29:07 -0300

    feat: router.js

---
+++ vue-router/src/router.js
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import Home from './views/Home.vue';
+import Sobre from './views/Sobre.vue';
+
+
+Vue.use(Router);
+
+export default new Router({
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: "/", 
+            name: "home", 
+            component: Home 
+        },
+        {
+            path: "/sobre",
+            name: "sobre",
+            component: Sobre
+        },
+    ]
+});
