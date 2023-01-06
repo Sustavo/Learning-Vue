b5038cb207423e536dde4122d63b1308aed1565c
Gustavo <gust4v0309@gmail.com>
2023-01-05 23:18:39 -0300

    Feat: Cursos.vue

---
+++ vue-router/src/views/Cursos.vue
@@ -0,0 +1,21 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>
+    <h1>Curso ativo: {{curso}}</h1>
+    <router-link :to="{name: 'curso', params: { curso: 'javascript' }}">JavaScript</router-link>
+    <router-link to="/cursos/html">HTML</router-link>
+    <router-link to="/cursos/css">CSS</router-link>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["curso"],
+  
+};
+</script>
+
+<style>
+</style>
