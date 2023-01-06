a718ee39b04de92e2af8eb746473e2503ea93ce1
Gustavo <gust4v0309@gmail.com>
2023-01-05 23:17:44 -0300

    feat: add diferent routers in app.vue

--- vue-router/src/App.vue
+++ vue-router/src/App.vue
@@ -1,21 +1,44 @@
 <template>
   <div id="app">
+    <h1 @click="mostrarRouter">Logo</h1>
     <nav>
-      <!-- Define o link para o router -->
       <router-link to="/">Home</router-link>
-      <router-link to="/sobre">Cursos</router-link>
+      <router-link to="/cursos">Cursos</router-link>
     </nav>
-    <!-- Local em que o componente aparecerá -->
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  methods: {
+    mostrarRouter() {
+      console.log(this.$route);
+    }
+  }
+};
 </script>
 
 <style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  color: #234;
+  border: 4px solid;
+  margin: 20px;
+  padding: 20px;
+}
 
+nav {
+  padding: 20px 0;
+}
+
+a {
+  padding: 10px;
+  color: #234;
+}
+
+.router-link-exact-active {
+  color: #4b8;
+  font-weight: bold;
+}
 </style>
