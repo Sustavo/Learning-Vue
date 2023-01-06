c430682d0595c2cb0262de83d6ca5bca6432f677
Gustavo <gust4v0309@gmail.com>
2023-01-05 23:18:14 -0300

    Feat: Curso.vue

---
+++ vue-router/src/views/Curso.vue
@@ -0,0 +1,31 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>
+    <h1>{{curso}}</h1>
+    <router-link :to="{name: 'descricao'}">Descrição</router-link>
+    <router-link :to="{name: 'aulas'}">Aulas</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["curso"],
+  methods: {
+    puxarDados() {
+      console.log("puxei api")
+    }
+  },
+  beforeRouteEnter(to, from, next) {
+    console.log(this)
+    next()
+  },
+  beforeRouteUpdate(to, from, next) {
+    this.puxarDados()
+    next()
+  }
+};
+</script>
+
+<style>
+</style>
