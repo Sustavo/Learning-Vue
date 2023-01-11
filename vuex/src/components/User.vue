bdf506487568550a124a53d4d53a83d1debbd78c
Gustavo <gust4v0309@gmail.com>
2023-01-11 13:25:27 -0300

    Feat: User.vue

---
+++ vuex/src/components/User.vue
@@ -0,0 +1,19 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <p>User {{ aulasCompletas }}</p>
+</template>
+
+<!-- eslint-disable vue/multi-word-component-names -->
+<script>
+export default {
+    computed: {
+        aulasCompletas() {
+            return this.$store.state.aulasCompletas;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
