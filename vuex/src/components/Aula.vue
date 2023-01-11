f5bf6189bfa5a8d8fe64c13093fc6a86eb4ddf7c
Gustavo <gust4v0309@gmail.com>
2023-01-11 13:25:20 -0300

    Feat: Aula.Vue

---
+++ vuex/src/components/Aula.vue
@@ -0,0 +1,37 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div>
+        <p>Aula {{ totalAulas }}, esse é o usuário: {{ user }}, {{ aulasCompletas }}</p>
+        <button @click="mostrarConsole">Mostrar no console</button>
+    </div>
+</template>
+
+<!-- eslint-disable vue/multi-word-component-names -->
+<script>
+import { mapState } from 'vuex';
+
+export default {
+    data() {
+        return {
+            totalAulas: 25,
+            nome: "André"
+
+        }
+    },
+    computed: {
+        ...mapState(["user", "aulasCompletas"]),
+        nomeMaiusculo() {
+            return this.nome.toUpperCase();
+        }
+    },  
+    methods: {
+        mostrarConsole() {
+            console.log(this.user)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
