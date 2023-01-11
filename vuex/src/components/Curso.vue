2d99397309330abc27ddfbef380641bc16e4ed50
Gustavo <gust4v0309@gmail.com>
2023-01-11 13:25:24 -0300

    Feat: Curso

---
+++ vuex/src/components/Curso.vue
@@ -0,0 +1,23 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div>
+        <p>Curso</p>
+        <Aula />
+    </div>
+</template>
+
+<!-- eslint-disable vue/multi-word-component-names -->
+<script>
+import Aula from './Aula.vue';
+
+export default {
+    name: 'Curso',
+    components: {
+        Aula
+    }
+}
+</script>
+
+<style>
+
+</style>
