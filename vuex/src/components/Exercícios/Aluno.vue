206f3b44512ab5fa3e42aa62bcc5cde242d5accd
Gustavo <gust4v0309@gmail.com>
2023-01-16 20:13:00 -0300

    Feat: Aluno.vue

---
+++ vuex/src/components/Exercícios/Aluno.vue
@@ -0,0 +1,17 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div>
+  
+    </div>
+  </template>
+  
+  <!-- eslint-disable vue/multi-word-component-names -->
+  <script>
+  export default {
+  
+  }
+  </script>
+  
+  <style>
+  
+  </style>
