811f96ac341d7942c21df5faf484649a0569bb90
Gustavo <gust4v0309@gmail.com>
2023-01-17 22:24:04 -0300

    Feat: store.js

--- vuex/src/store.js
+++ vuex/src/store.js
@@ -7,6 +7,25 @@ export default new Vuex.Store({
     state: {
         user: "Lobo",
         aulasCompletas: 10,
+        aulasCompletasExec: [],
+        acaoExec: null,
+        livros: [
+            {
+              nome: "O Senhor dos Anéis",
+              lido: true,
+            },
+            {
+              nome: "Harry Potter",
+              lido: true,
+            },
+            {
+              nome: "As Crônicas de Gelo e Fogo",
+              lido: false,
+            }
+          ]
+    },
+    getters: {
+        livrosLidos: state => lido => state.livros.filter(livros => livros.lido === lido)
     },
     mutations: {
         CHANGE_USER(state, payload) {
@@ -15,6 +34,29 @@ export default new Vuex.Store({
         },
         COMPLETAR_AULA(state) {
             state.aulasCompletas++
+        },
+        COMPLETAR_AULA_EXERC(state,payload) {
+            state.aulasCompletasExec.push(payload)
+        },
+        UPDATE_ACAO(state, payload) {
+            state.acaoExec = payload;
+        }
+    },
+    actions: {
+        completarAula(context, payload) {
+            context.commit("COMPLETAR_AULA_EXERC", payload)
+        },
+
+        puxarAcao(context) {
+            fetch('https://api.origamid.dev/stock/aapl/quote')
+            .then(r => r.json())
+            .then(r => {
+                console.log(context)
+                context.commit("UPDATE_ACAO", r);
+                context.dispatch("completarAula", {
+                    aula: "Apple"
+                })
+            })
         }
     }
 }) 
