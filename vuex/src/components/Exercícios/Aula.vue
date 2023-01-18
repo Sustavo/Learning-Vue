f163b33ef73a3b44ed87c3cd52de6a8d9656cdf1
Gustavo <gust4v0309@gmail.com>
2023-01-17 22:24:22 -0300

    Feat: Aula.vue

--- vuex/src/components/Exercícios/Aula.vue
+++ vuex/src/components/Exercícios/Aula.vue
@@ -1,14 +1,43 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div>
-
+    <p>Aulas</p>
+    <ul>
+      <li v-for="aula in aulas" :key="aula.nome">
+        <p>{{ aula.nome }} | {{ aula.duracao }} horas</p>
+        <button @click="completarAula(aula)">Completar Aula</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <!-- eslint-disable vue/multi-word-component-names -->
 <script>
-export default {
+import { mapActions } from 'vuex';
 
+export default {
+  data() {
+    return {
+      aulas: [
+          {
+            nome: "HTML e CSS",
+            duracao: 15
+          },
+          {
+            nome: "JavaScript",
+            duracao: 30
+          },
+          {
+            nome: "UX Design",
+            duracao: 20
+          }
+      ]
+    }
+  },
+  methods: {
+    ...mapActions(["completarAula"]),
+    
+  }
 }
 </script>
 
