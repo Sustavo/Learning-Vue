b8bb36c76026649c7ea53298f38a7bb313d071e3
Gustavo <gust4v0309@gmail.com>
2023-01-17 22:24:17 -0300

    Feat: Aluno.vue

--- vuex/src/components/Exercícios/Aluno.vue
+++ vuex/src/components/Exercícios/Aluno.vue
@@ -1,14 +1,18 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
     <div>
-  
+      <p>Aluno {{ aulasCompletasExec }}</p>
     </div>
   </template>
   
   <!-- eslint-disable vue/multi-word-component-names -->
   <script>
+  import { mapState } from 'vuex';
+
   export default {
-  
+    computed: {
+      ...mapState(["aulasCompletasExec"]),
+    }
   }
   </script>
   
