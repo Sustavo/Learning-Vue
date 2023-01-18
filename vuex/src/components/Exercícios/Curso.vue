e2160ba802206af4e4f88b925fdffa71cf135bd5
Gustavo <gust4v0309@gmail.com>
2023-01-17 22:24:26 -0300

    Feat: Curso.vue

--- vuex/src/components/Exercícios/Curso.vue
+++ vuex/src/components/Exercícios/Curso.vue
@@ -1,14 +1,19 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
     <div>
-  
+      <p>Curso de JavaScript</p>
+      <Aulas />
     </div>
   </template>
   
   <!-- eslint-disable vue/multi-word-component-names -->
   <script>
+  import Aulas from '@/components/Exercícios/Aula.vue'
+
   export default {
-  
+    components: {
+      Aulas
+    }
   }
   </script>
   
